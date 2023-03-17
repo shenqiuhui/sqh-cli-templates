7b09929a5640f22a61c28e2151c3985857b0338a
shenqiuhui <pandashen821@163.com>
2023-03-17 21:30:46 +0800

    feat: sqh-cli-vue2-page-template

---
+++ sqh-cli-vue2-page-template/template/Snippet/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="<%= paramCaseName %>">
+    <h1><%= name %></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '<%= name %>'
+}
+</script>
+
+<style scoped>
+.<%= paramCaseName %> {
+  width: 100%;
+  height: 100%;
+}
+</style>
